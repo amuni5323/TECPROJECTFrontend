78a155dde4b86d5fad5498d71b079f7f2db65e25
amuni5323 <amuni11.hamai@gmail.com>
2025-06-30 16:53:49 +0300

    fixed

--- src/app/verify-email/page.js
+++ src/app/verify-email/page.js
@@ -24,6 +24,40 @@
 //     </div>
 //   );
 // }
+// "use client";
+
+// import { useEffect, useState } from 'react';
+// import { useRouter, useSearchParams } from 'next/navigation';
+
+// export default function VerifyEmail() {
+//   const router = useRouter();
+//   const searchParams = useSearchParams();
+//   const token = searchParams.get('token');
+//   const [message, setMessage] = useState('');
+
+//   useEffect(() => {
+//     if (token) {
+//       fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/auth/verify-email/${token}`)
+//         .then(async (res) => {
+//           const data = await res.json();
+//           if (res.ok) {
+//             setMessage(data.message);
+//           } else {
+//             setMessage(data.message || 'Error verifying email');
+//           }
+//         })
+//         .catch(() => setMessage('Network error'));
+//     }
+//   }, [token]);
+
+//   return (
+//     <div className="text-center mt-20">
+//       <h2 className="text-2xl font-bold">Email Verification</h2>
+//       <p className="mt-4 text-lg text-green-600">{message}</p>
+//     </div>
+//   );
+// }
+
 "use client";
 
 import { useEffect, useState } from 'react';
